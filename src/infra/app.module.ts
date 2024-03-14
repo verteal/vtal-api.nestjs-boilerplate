568b0bd55b46b39cf437cb3430a5d9e7ee12bb33
fgouvea10 <felipegalvesouvea@gmail.com>
2024-03-14 19:20:48 -0300

    feat: remove typeorm and add prisma

--- src/infra/app.module.ts
+++ src/infra/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common'
-import { TypeOrmModule } from '@nestjs/typeorm'
+import { ConfigModule } from '@nestjs/config'
 
 import { EnvModule } from './env/env.module'
-import { EnvService } from './env/env.service'
 import { RabbitMQModule } from './rabbitmq/rabbitmq.module'
-import { ConfigModule } from '@nestjs/config'
 import { envSchema } from './env/env'
 
 @Module({
@@ -12,20 +10,6 @@ import { envSchema } from './env/env'
     ConfigModule.forRoot({
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
-    }),
-    TypeOrmModule.forRootAsync({
-      imports: [EnvModule],
-      inject: [EnvService],
-      useFactory: (envService: EnvService) => ({
-        type: 'postgres',
-        port: envService.get('DATABASE_PORT'),
-        username: envService.get('DATABASE_USERNAME'),
-        password: envService.get('DATABASE_PASSWORD'),
-        database: envService.get('DATABASE_NAME'),
-        entities: [],
-        synchronize: true,
-        logging: true,
-      }),
     }),
     EnvModule,
     RabbitMQModule,
